7ad0bd0af726be76f4413c7dd92ca1e4dc41041f
Ryûga <corentingambier@gmail.com>
2023-10-03 09:03:02 +0200

    com

---
+++ cmake-build-debug/CMakeFiles/sysinfo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sysinfo.dir/main.c.obj"
+  "CMakeFiles/sysinfo.dir/main.c.obj.d"
+  "libsysinfo.dll.a"
+  "sysinfo.exe"
+  "sysinfo.exe.manifest"
+  "sysinfo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sysinfo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <systemtopologyapi.h>
+#include <sysinfoapi.h>
+
+int main() {
+    printf("Hello, World!\n");
+    sys_errlist;
+    system()
+    return 0;
+}
